0c4660d9d0e4875a988270fb045d23847c039b3b
Xueron Ni <xueron@xueron.com>
2018-04-29 21:15:40 +0800

    update

--- 3.3.0/Phalcon/Forms/Form.php
+++ 3.3.0/Phalcon/Forms/Form.php
@@ -127,7 +127,27 @@ namespace Phalcon\Forms {
 
 
 		/**
-		 * Returns the messages generated in the validation
+		 * Returns the messages generated in the validation.
+		 *
+		 * <code>
+		 * if ($form->isValid($_POST) == false) {
+		 *     // Get messages separated by the item name
+		 *     // $messages is an array of Group object
+		 *     $messages = $form->getMessages(true);
+		 *
+		 *     foreach ($messages as $message) {
+		 *         echo $message, "<br>";
+		 *     }
+		 *
+		 *     // Default behavior.
+		 *     // $messages is a Group object
+		 *     $messages = $form->getMessages();
+		 *
+		 *     foreach ($messages as $message) {
+		 *         echo $message, "<br>";
+		 *     }
+		 * }
+		 * </code>
 		 */
 		public function getMessages($byItemName=null){ }
 
